77085d51e23b012b055979444eab7624d13d6ed8
Calvin <43643839+bot1324@users.noreply.github.com>
2019-09-28 19:46:07 -0500

    Tier 1 Tasks Complete

--- Assets/Scripts/Enemy/TrainingDummy.cs
+++ Assets/Scripts/Enemy/TrainingDummy.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class TrainingDummy : MonoBehaviour
+public class TrainingDummy : MonoBehaviour, IEnemy
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float DisplayTime;
+    public float StartBuffer;
+    public TextMeshPro DamageText;
+    public ParticleSystem ParticleEffects;
 
-    // Update is called once per frame
+    private int _total_damage;
+    private float _last_hit_time;
+    private float _start_time;
+
     void Update()
     {
-        
+        if (_last_hit_time + DisplayTime < Time.time)
+        {
+            _total_damage = 0;
+            DamageText.text = "0";
+        }
+    }
+
+    public void OnHit(int damage, bool stun)
+    {
+        _total_damage += damage;
+        if (_last_hit_time + DisplayTime < Time.time)
+            _start_time = Time.time - StartBuffer;
+        _last_hit_time = Time.time;
+
+        float time = Time.time - _start_time;
+        time = time <= StartBuffer ? 1 : time;
+        DamageText.text = ((int) (_total_damage / time)).ToString();
+    }
+
+    public ParticleSystem GetParticles()
+    {
+        return ParticleEffects;
     }
 }
