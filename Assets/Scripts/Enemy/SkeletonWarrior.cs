2a6a420c2892f610b36b4d7736d43c6ce217dbce
Calvin <43643839+bot1324@users.noreply.github.com>
2019-11-22 08:29:25 -0600

    New enemies and animations

---
+++ Assets/Scripts/Enemy/SkeletonWarrior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonWarrior : MonoBehaviour
+{
+    Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
