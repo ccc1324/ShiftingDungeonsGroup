f227dacbae87f67ce47c9e1767f522e2ba6ad9e9
Calvin <43643839+bot1324@users.noreply.github.com>
2020-02-07 23:42:31 -0600

    Added Spikes
    *New Sprites and Prefacs
    *Also modified EnemyAttackTrigger for one-way detection

--- Assets/Scripts/Enemy/EnemyAttackTrigger.cs
+++ Assets/Scripts/Enemy/EnemyAttackTrigger.cs
@@ -10,19 +10,45 @@ public class EnemyAttackTrigger : MonoBehaviour
     public ParticleSystem ParticleEffects;
     public int Damage = 1;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public enum Direction { None, Left, Right, Up, Down}
+    public Direction OneWay;
+    public Collider2D OneWayTrigger; //only needed if the trigger needs to be OneWay
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.tag == "Player" && 
-            !collision.GetComponent<PlayerStun>().ParalyzeHeal &&   //avoid damage during/after stun
-            collision.GetComponent<PlayerCombat>().enabled == true) //avoid additional damage on same frame after being hit once
+        if (other.tag == "Player" && 
+            !other.GetComponent<PlayerStun>().ParalyzeHeal &&   //avoid damage during/after stun
+            other.GetComponent<PlayerCombat>().enabled == true) //avoid additional damage on same frame after being hit once
         {
-            collision.GetComponent<PlayerMovement>().Stop();
-            collision.GetComponent<Animator>().SetTrigger("Stun");
-            collision.GetComponent<Animator>().SetBool("Attacking", false);
+            //Check for OneWay Condition
+            if (OneWay != Direction.None)
+            {
+                if (OneWay == Direction.Left)
+                    if (other.transform.position.x + other.offset.x > transform.position.x + OneWayTrigger.offset.x)
+                        return;
+                if (OneWay == Direction.Right)
+                {
+                    float a = other.transform.position.x + other.offset.x;
+                    float b = transform.position.x + other.offset.x;
+                    if (other.transform.position.x + other.offset.x < transform.position.x + OneWayTrigger.offset.x)
+                        return;
+                }
+                    
+                if (OneWay == Direction.Up)
+                    if (other.transform.position.y + other.offset.y < transform.position.y + OneWayTrigger.offset.y)
+                        return;
+                if (OneWay == Direction.Down)
+                    if (other.transform.position.y + other.offset.y > transform.position.y + OneWayTrigger.offset.y)
+                        return;
+            }
+
+            other.GetComponent<PlayerMovement>().Stop();
+            other.GetComponent<Animator>().SetTrigger("Stun");
+            other.GetComponent<Animator>().SetBool("Attacking", false);
 
             //Disables PlayerCombat and re-enables after 0.1 seconds, used to avoid some issues with stunning the player
-            collision.GetComponent<PlayerStun>().DisablePlayerCombat(0.1f); 
-            collision.GetComponent<Player>().OnHit(Damage);
+            other.GetComponent<PlayerStun>().DisablePlayerCombat(0.1f); 
+            other.GetComponent<Player>().OnHit(Damage);
 
             if (tag == "Projectile")
             {
@@ -31,7 +57,7 @@ public class EnemyAttackTrigger : MonoBehaviour
             }
         }
 
-        if (collision.tag == "Room" || collision.tag == "Wall")
+        if (other.tag == "Room" || other.tag == "Wall")
         {
             if (tag == "Projectile")
                 Destroy(gameObject);
